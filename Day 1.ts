88e98d1acad881c4a85b99ad17eac80f349aef76
DanielAlfaro2222 <kdalfaro45@misena.edu.co>
2022-12-02 21:07:39 -0500

    Chore: Se sube la solucion de los primeros retos

---
+++ Day 1.ts
@@ -0,0 +1,39 @@
+/*  Problema dia 1
+
+    Este año los elfos han comprado una máquina que envuelve regalos. Pero… ¡no viene programada! Necesitamos crear un algoritmo que le ayude en la tarea.
+
+    A la máquina se le pasa un array con los regalos. Cada regalo es un string. Necesitamos que la máquina envuelva cada regalo en papel de regalo y lo coloque en un array de regalos envueltos.
+
+    El papel de regalo es el símbolo * y para envolver un regalo se coloca el símbolo * de forma que rodee totalmente al string por todos los lados. Por ejemplo:
+
+    const gifts = ['cat', 'game', 'socks']
+    const wrapped = wrapping(gifts)
+
+    console.log(wrapped)
+    [
+        "*****\\n*cat*\\n*****",
+        "******\\n*game*\\n******",
+        "*******\\n*socks*\\n*******"
+    ]
+
+    Como ves, el papel de regalo envuelve el string. Por arriba y por abajo, para no dejar ningún hueco, las esquinas también están cubiertas por el papel de regalo.
+
+    Nota: El carácter \n representa un salto de línea.
+
+    ¡Ojo! Asegúrate que pones el número correcto de * para envolver completamente el string. Pero no demasiados. Sólo los necesarios para cubrir el string.
+*/
+
+function wrapping(gifts: any): string[] {
+  gifts = gifts.map((gift) => {
+    const size: number = gift.length + 2;
+    const asterisk: string = createAsterisk(size);
+
+    return `${asterisk}\n*${gift}*\n${asterisk}`;
+  });
+
+  return gifts;
+}
+
+function createAsterisk(size: number): string {
+  return "*".repeat(size);
+}
